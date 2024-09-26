fabe76e51e35f4fbb6d60f80080d83befe3033d6
JuanPGO <pablogomez.jg@gmail.com>
2024-09-26 15:53:57 -0500

    Organized the App.tsx

--- Ulimaps/src/views/App.tsx
+++ Ulimaps/src/views/App.tsx
@@ -7,7 +7,6 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<WelcomeInterface />} />
-          
         </Routes>
       </div>
     </Router>
