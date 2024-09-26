1bae17d93f188f4f2a646eb650f40f39eb5cd3ed
saraccantillo <saraccantillo@gmail.com>
2024-09-26 10:22:46 -0500

    Una opcion de interfaz nueva

--- Ulimaps/src/views/welcome.tsx
+++ Ulimaps/src/views/welcome.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
 import '../styles/welcome.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
+
+
 
 const WelcomeInterface: React.FC = () => {
     return (
         <div className="contenedor-bienvenida">
-            <nav className="navegacion">
-                <button className="boton-nav">
-                    <img src="./src/assets/img/botones/gps.png" alt="Navegación" />
-                </button>
-            </nav>
-            <header className="encabezado">
-                <button className="boton-logo">
-                    <img src="./src/assets/img/botones/escudo.png" alt="Escudo Unilibre" />
-                </button>
-            </header>
-            <main className="contenido-principal">
-                <h1 className="titulo">Bienvenido</h1>
-                <img className="imagen-bienvenida" src="./src/assets/img/fotos/fotoBienvenida.jpg" alt="Foto Bienvenida" />
-                <h2 className="subtitulo">Campus Belmonte, Pereira</h2>
-            </main>
+
+            <div className='contenedor-interior1'>
+
+                <div className="divNavegacion">
+
+                    <nav className="navegacion">
+                        <button className="boton-nav">
+                            
+                            <FontAwesomeIcon icon={faMapLocationDot} style={{color: "#ffffff",}} />
+                            
+                        </button>
+                    </nav>
+
+                </div>
+
+                <div className="divEncabezado">
+
+                    <header className="encabezado">
+                        <div className="boton-logo">
+                            <img src="./src/assets/img/fotos/escudo.png" alt="Escudo Unilibre" />
+                        </div>
+                    </header>
+
+                </div>
+
+                <div className="divBienvenida">
+
+                    <main>
+
+                        <h1 className="titulo">Bienvenido</h1>
+                        <h2 className="subtitulo">Campus Belmonte, Pereira</h2>
+
+                    </main>
+
+                </div>
+
+            </div>
+            
+            <div className="contenedor-interior2">
+
+                <div className="imagenCampus">
+
+                        <img src="./src/assets/img/fotos/fotoBienvenida.jpg" alt="" />                
+                </div>
+
+            </div>
+            
         </div>
     );
 };
