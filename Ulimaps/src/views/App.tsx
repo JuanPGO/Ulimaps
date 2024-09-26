9c57f7e5a89c0ccb14f9c10d8544ad5c37345cf0
JuanPGO <pablogomez.jg@gmail.com>
2024-09-26 14:46:06 -0500

    The App.tsx file is restored, route management is added, and button labels are changed to Link

---
+++ Ulimaps/src/views/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WelcomeInterface from './welcome.tsx';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<WelcomeInterface />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
