b69c78c997d46fda150af84af62bc3df4e0e636b
JuanPGO <pablogomez.jg@gmail.com>
2024-10-03 09:07:35 -0500

    classes were create

---
+++ Ulimaps/src/models/PuntoInterior.ts
@@ -0,0 +1,10 @@
+
+export class PuntoInterior {
+    tipo:string;
+    nombre:string;
+    // visible:Mapa;
+    constructor(tipo:string,nombre:string){
+        this.tipo = tipo
+        this.nombre = nombre
+    }
+}
